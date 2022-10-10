41708389c9e181cb0cf62930d09696aecdf945ae
mahfuzhur <>
2022-10-10 11:59:00 +0600

    initial

---
+++ app/Image.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $guarded = [];
+    public function image(){
+        return $this->morphTo(Image::class,'imageable');
+    }
+}
